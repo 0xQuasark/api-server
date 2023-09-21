d2139fc0c22054880deb17fabfbf99279630c6c1
Quasark <q@quasark.xyz>
2023-09-21 10:43:50 -0700

    sqlite connection string added for github workflow test

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const FoodConnection = require('./food.js');
 const ClothesConnection = require('./clothes.js');
 
-const SQL_CONNECTION_STRING = process.env.SQL_CONNECTION_STRING;
+const SQL_CONNECTION_STRING = process.env.SQL_CONNECTION_STRING || 'sqlite:memory:';
 
 console.log(SQL_CONNECTION_STRING);
 const sequelize = new Sequelize(SQL_CONNECTION_STRING, { dialect: 'postgres' }); // this is a singleton.
